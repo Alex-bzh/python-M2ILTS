91648e4e8440074f882a0d81524fdccfd62638e4
Alexandre Roulois <alexandre@roulois.fr>
2022-12-16 16:57:26 +0100

    Add comments

--- scripts/books_in_library.py
+++ scripts/books_in_library.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+# module to parse XML files
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('../data/library.xml')
+# which file?
+tree = ET.parse('../data/XML/library.xml')
+# get root element
 root = tree.getroot()
 
+# from root, find every book elements
+# and parse each one
 for book in tree.iter('book'):
 
+    # a separator between items
     print('=' * 50)
 
+    # ref_author attribute helps find infos about author
     ref_author = book.attrib['ref_author']
 
+    # from root, find every author elements
+    # and parse each one
     for author in tree.iter('author'):
+        # check if current ref_author is one of id_author
         if author.attrib['id_author'] == ref_author:
+            # if so, means this author is linked to book element
             firstname = author.find('firstName').text
             lastname = author.find('lastName').text
 
+    # inner text of title and publication elements
     title = book.find('title').text
     pub_date = book.find('publication').text
     
+    # print result
     print(f"Author: {firstname} {lastname}\nTitle: {title}\nPublication date: {pub_date}")

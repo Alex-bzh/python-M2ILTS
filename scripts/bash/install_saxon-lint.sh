2b4c280779eae2e0ee8870a9b38f169c37e5e678
Alexandre Roulois <alexandre@roulois.fr>
2022-12-30 11:50:04 +0100

    shortcut to saxon-lint

--- scripts/bash/install_saxon-lint.sh
+++ scripts/bash/install_saxon-lint.sh
@@ -4,3 +4,11 @@
 git clone https://github.com/sputnick-dev/saxon-lint.git
 cd saxon-lint*
 chmod +x saxon-lint.pl
+
+# add saxon-lint.pl to PATH variable
+echo "PATH=$PATH:~/scripts/bash/saxon-lint" >> ~/.bashrc
+
+# make an alias
+echo "alias saxon-lint='saxon-lint.pl'" >> ~/.bashrc
+
+source ~/.bashrc

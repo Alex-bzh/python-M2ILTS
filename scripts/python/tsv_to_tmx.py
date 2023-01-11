1888029b2ea7b817233fdca2046ec25b50bf4771
Alexandre Roulois <alexandre@roulois.fr>
2023-01-11 08:34:19 +0100

    Convert TSV to TMX

---
+++ scripts/python/tsv_to_tmx.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#
+# modules to import
+#
+
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+import csv
+
+#
+# user functions
+#
+
+def pretty_print(xml, path):
+	"""Pretty print XML tree into a TMX file
+
+	xml_tree -- an ElementTree object
+	"""
+	xmlstr = minidom.parseString(ET.tostring(xml)).toprettyxml()
+
+	with open(path, "w") as xmlfile:
+		xmlfile.write(xmlstr)
+
+# main function
+def main(src, dest):
+
+	# <root> element
+	root = ET.Element("tmx")
+
+	# <header> element
+	header = ET.SubElement(root, "header", {
+		"creationtool": str(),
+		"creationtoolversion": str(),
+		"segtype": str(),
+		"o-tmf": str(),
+		"adminlang": str(),
+		"srclang": str(),
+		"datatype": str()
+	})
+
+	# <body> element
+	body = ET.SubElement(root, "body")
+
+	# data is in TSV file
+	with open(f"../../data/CSV/{ src }") as csvfile:
+		# reading data with appropriate module
+		reader = csv.DictReader(csvfile, delimiter="\t")
+
+		# for each row in TSV file
+		for idx, row in enumerate(reader):
+			# create a <tu> element
+			tu = ET.SubElement(body, "tu", { "tuid": f"s{ idx }"})
+
+			# for each column (a variant in a particular language)
+			for lang, segment in row.items():
+				# create <tuv> and <seg> elements
+				tuv = ET.SubElement(tu, "tuv", { "xml:lang": lang })
+				seg = ET.SubElement(tuv, "seg")
+				seg.text = segment
+
+	# XML tree into a TMX file
+	pretty_print(root, f"../../data/XML/{ dest }")
+
+# program
+if __name__ == "__main__":
+
+	# file names
+	csvfile = "ftb_N+A.tsv"
+	tmxfile = "ftb_N+A.tmx"
+
+	# execute main procedure
+	main(csvfile, tmxfile)
